479873fd79000e8e725473f708a399bc1c4d05a8
tanihito <tanihito7@gmail.com>
2015-12-19 18:28:05 +0900

    Add Brewfile.sh

---
+++ Brewfile.sh
@@ -0,0 +1,30 @@
+brew update
+
+brew install git
+brew install go
+brew install jq
+brew install rbenv
+brew install ruby-build
+brew install readline
+brew install tig
+brew install tree
+brew install zsh
+
+brew install caskroom/cask/brew-cask
+brew cask install alfred
+brew cask install appcleaner
+brew cask install bettertouchtool
+brew cask install coteditor
+brew cask install firefox
+brew cask install java
+brew cask install karabiner
+brew cask install gitx
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install hipchat
+brew cask install iterm2
+brew cask install skype
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install xtrafinder
+brew cask alfred link
